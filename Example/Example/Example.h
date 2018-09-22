daa8b77f0086b62219bfb708d05c4df8ba8b2815
isaoeka <isaoeka@gmail.com>
2018-09-22 20:02:43 +0900

    :+1: Add Xcode project

---
+++ Example/Example/Example.h
@@ -0,0 +1,19 @@
+//
+//  Example.h
+//  Example
+//
+//  Created by isaoeka on 2018/09/22.
+//  Copyright © 2018年 isaoeka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Example.
+FOUNDATION_EXPORT double ExampleVersionNumber;
+
+//! Project version string for Example.
+FOUNDATION_EXPORT const unsigned char ExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Example/PublicHeader.h>
+
+
---
+++ Kiriban/Kiriban.swift
@@ -0,0 +1,12 @@
+//
+//  Kiriban.swift
+//  Example
+//
+//  Created by isaoeka on 2018/09/22.
+//  Copyright © 2018年 isaoeka. All rights reserved.
+//
+
+import UIKit
+
+public extension Int {
+}
